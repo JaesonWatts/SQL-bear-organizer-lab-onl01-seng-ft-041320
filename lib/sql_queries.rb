45671a40b4b6daf62c5ea2e60827ed7ad161060d
Jaeson Watts <jaeson.watts@gmail.com>
2020-05-12 07:20:08 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,7 +1,7 @@
 def selects_all_female_bears_return_name_and_age
   "SELECT bears.name, bears.age
    FROM bears 
-   WHERE (gender = 'female');"
+   WHERE (gender='female');"
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
