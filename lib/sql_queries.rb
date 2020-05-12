6b910d9f2c4e52c8e39dc1e8ce5aad913ea03ac6
Jaeson Watts <jaeson.watts@gmail.com>
2020-05-12 07:38:02 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -41,7 +41,7 @@ end
 def counts_number_of_bears_with_goofy_temperaments
   "SELECT COUNT(temperament) 
   FROM bears
-  WHERE temperament='goofy;"
+  WHERE temperament='goofy';"
 end
 
 def selects_bear_that_killed_Tim
